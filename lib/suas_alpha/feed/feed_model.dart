9a8ba4931bbebb6f9386fbf63d529005b47b51df
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-01 01:43:59 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/suas_alpha/feed/feed_model.dart
@@ -0,0 +1,34 @@
+import '/auth/supabase_auth/auth_util.dart';
+import '/backend/supabase/supabase.dart';
+import '/components/category_tag_card_widget.dart';
+import '/components/drawer_content_widget.dart';
+import '/components/empty_list_post_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class FeedModel extends FlutterFlowModel {
+  ///  State fields for stateful widgets in this page.
+
+  // Model for drawerContent component.
+  late DrawerContentModel drawerContentModel;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {
+    drawerContentModel = createModel(context, () => DrawerContentModel());
+  }
+
+  void dispose() {
+    drawerContentModel.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
